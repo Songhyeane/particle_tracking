d1898e8f5af9ec90cf730a3c9fe5186d3d99c474
Songhyeane <line9988@gachon.ac.kr>
2023-03-23 12:48:35 +0900

    Initial commit

---
+++ datas.py
@@ -0,0 +1,26 @@
+class data :
+    pass
+
+class tiffs(data) :
+    def __init__(self,name):
+        self.name = name
+
+    def set_cropped_tiffs(self,image,file_path):
+        pass
+
+    def set_r_cropped_tiffs(self,image,file_path):
+        pass
+
+class paricle(data):
+    def __int__(self,name):
+        self.name = name
+
+    def tracked_particle(self,image,file_path):
+        pass
+
+    def linear_particle(self,image,file_path):
+        pass
+
+    def non_linear_particle(self,image,file_path):
+        pass
+
---
+++ librarys.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets  import RectangleSelector
+import tifffile as tiff
+from PIL import Image
+import pims
+import os
